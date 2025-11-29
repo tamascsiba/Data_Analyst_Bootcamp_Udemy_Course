8a5966a619f8d6697e690b2fe11a97a795b60b7b
Tamas_Csiba <csiba.toma1@gmail.com>
2025-11-29 13:43:04 +0100

    import

---
+++ Section_2-3_Python/5_Import/package/__init__.py
@@ -0,0 +1,1 @@
+""" init.py is a special file used in Python to define packages and initialize their namespaces """ 
---
+++ Section_2-3_Python/5_Import/test.py
@@ -0,0 +1,7 @@
+from package.maths import *
+from package.subpackages.mul import multiply
+
+print(addition(2,3))
+print(substraction(4,3))
+
+print(multiply(5,6))
